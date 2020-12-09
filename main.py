f11ff8ee1944b1f09ef22a555ca8849ff15d15d0
JoaoP <contato.joaocomp@gmail.com>
2020-12-09 18:28:19 -0300

    Finishing fibonacci feature logic

--- main.py
+++ main.py
@@ -14,5 +14,11 @@ def div(a, b):
 def mul(a, b):
 	return a * b
 
+def fibonacci(n):
+	if n <= 1:
+		return 1
+	else:
+		return fibonacci(n - 1) + fibonacci(n - 2)
+
 if __name__ == '__main__':
 	print("Nothing to see here")
